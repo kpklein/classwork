0b1682bb3647c17969b66297a3c38dbd4703b9a7
Tech-A38 <Tech-A38@Tech-A38s-MacBook-Air.local>
2017-09-27 10:57:39 +0100

    updated sinatra

--- w04d02/sinatra_intro/app.rb
+++ w04d02/sinatra_intro/app.rb
@@ -1,29 +1,40 @@
 require 'sinatra'
+require_relative 'sinatra/reloader' if development?
 require_relative "./TodoManager.rb"
 
 
+
+# INDEX - all the data
 get '/todos/:id' do
 
 	TodoManager.index
 end
 
+# CREATE - create a new todo
+post '/todos/:id' do
+	new_todo = params[:todo]
+	TodoManager.create(new_todo)
+	end
+
+# SHOW - show a single record
 get '/todos/:id' do
 
-	"CREATE"
+	id = parms[:id].to_i
+	TodoManager.show(id)
+	
 end
 
-get '/todos/:id' do
 
-	id = parms[:id] 
-	"SHOW"
-end
+# UPDATE - update a single record
+put '/todos/:id' do
+	id = parms[:id].to_i
+	updated_todo = parms[:todo]
+	TodoManager.update(id, updated_todo)
+	end
 
-get '/todos/:id' do
-	id = parms[:id] 
-	"UPDATE"
-end
-
-get '/todos/:id' do
-	id = parms[:id] 
-	"DELETE"
-end
+# DESTROY - delete a single record
+Delete 'todos/id' do
+	id = parms[:id].to_i
+	updated_todo = parms[:update] 
+	TodoManager.update(id, updated_todo)
+	end
